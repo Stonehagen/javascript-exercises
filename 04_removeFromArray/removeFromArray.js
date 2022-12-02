b6c5890fe08489ff43b5051e4b2375db56be4486
Stonehagen <steinhagen@mailbox.org>
2022-12-02 21:54:55 +0100

    do remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
 const removeFromArray = function() {
 
+    // get amount of arguments  and copy args to array
+    let lenArgs = arguments.length;
+    let remArgs = Array.prototype.slice.call(arguments, 1);
+    let array = [...arguments[0]];
+
+    if (lenArgs > 1 ) {
+        let newArray = array.filter(function(v, i, arr){
+            for( i = 1 ; i < lenArgs ; i++ ) {
+                if (v == remArgs[i-1]){
+                    return false;
+                }
+            }
+            return true;
+        })
+        return newArray;
+    }
 };
 
 // Do not edit below this line
