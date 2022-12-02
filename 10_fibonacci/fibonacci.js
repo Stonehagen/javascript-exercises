3f9990a1c9b4724586dc23e80dc1170fd1a2705c
Stonehagen <steinhagen@mailbox.org>
2022-12-02 23:29:52 +0100

    do fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0 ) {
+        return "OOPS";
+    }
+    let fibo = [1,1,];
+
+    for ( let i = 2 ; i < n ; i++ ) {
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+
+    return fibo[n-1];
 
 };
 
