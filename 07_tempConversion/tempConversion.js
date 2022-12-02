87a471f739266afb905057c7f4a3996e84cb330a
Stonehagen <steinhagen@mailbox.org>
2022-12-02 22:16:57 +0100

    do temp Conversation exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let cel = (fahr - 32) * ( 5/9 );
+  return Math.round(cel, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let fahr = (cel * ( 9/5 ) + 32);
+  return Math.round(fahr, 1);
 };
 
 // Do not edit below this line
