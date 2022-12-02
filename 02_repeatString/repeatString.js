f32656d75fba1021c94baac2772021c5382d5c4f
Stonehagen <steinhagen@mailbox.org>
2022-12-02 19:29:35 +0100

    make second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(s, n) {
+    let concatStr ="";
+    for (let i = 0; i < n; i++) {
+        concatStr += s;
+    }
+    return concatStr;
 };
 
 // Do not edit below this line
