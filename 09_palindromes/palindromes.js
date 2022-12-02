fbfd5eaf9085d7d3bb3fbe77bf8a692202824821
Stonehagen <steinhagen@mailbox.org>
2022-12-02 23:19:03 +0100

    do palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let revStr = str.split("").reverse().join("").replace(/[^\w]/mg, "");
+    let forStr = str.replace(/[^\w]/mg, "");
+    
+    console.log(revStr, forStr);
+    return forStr.toLowerCase() == revStr.toLowerCase();
 };
 
 // Do not edit below this line
