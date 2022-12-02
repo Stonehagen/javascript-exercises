dfbb30d82691a58f8b07910ddca146bac7d3c702
Stonehagen <steinhagen@mailbox.org>
2022-12-02 22:39:18 +0100

    do calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,48 @@
 const add = function() {
+	let sum = 0;
+  let numbers = [...arguments];
+  for ( i = 0 ; i < numbers.length ; i++ ) {
+    sum += numbers[i];
+  }
+  return sum;
+};
+
+const subtract = function(a, b) {
+  return a - b;
+  
+};
+
+const sum = function(arr) {
+  let numbers = [...arr];
+  let prod = 0;
+
+  for ( i = 0 ; i < numbers.length ; i++ ) {
+    prod += numbers[i];
+  }
+  return prod;
+};
+
+const multiply = function(arr) {
+  let numbers = [...arr];
+  let prod = numbers[0];
+
+  for ( i = 1 ; i < numbers.length ; i++ ) {
+    prod *= numbers[i];
+  }
+  return prod;
+};
+
+const power = function(x, y) {
+  return x ** y;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(n) {
+  let fac = 1;
+  for ( let i = n ; i > 0 ; i--) {
+    fac *= i;
+  }
+  return fac;
 	
 };
 
