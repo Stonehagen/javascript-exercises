5a5d4bd3630c4cfeb5484705c4cccf309f89ade5
Stonehagen <steinhagen@mailbox.org>
2022-12-02 20:17:58 +0100

    make third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let lenStr = str.length;
+    let revStr = "";
+    for (let i = lenStr ; i > 0 ; i--) {
+        revStr += str[i-1];
+    }
+    return revStr;
 };
 
 // Do not edit below this line
